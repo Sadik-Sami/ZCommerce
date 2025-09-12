f2322cfa186d7fc3b4072cf7bd6b6389845d8dd4
Sadik-Sami <miasimanto@gmail.com>
2025-09-13 02:46:28 +0600

    Json parsing and URL encoding middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,10 @@
-import express from 'express';
+import express, { json, urlencoded } from 'express';
 import productRoutes from './routes/products/index';
 const port = 3000;
 const app = express();
+
+app.use(urlencoded({ extended: false }));
+app.use(json());
 
 app.get('/', (req, res) => {
 	res.send('Hello World');
