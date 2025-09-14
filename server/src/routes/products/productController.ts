c486fdf3d36beb61926631c7f1d70d3a4c6c2217
Sadik-Sami <miasimanto@gmail.com>
2025-09-14 07:26:16 +0600

    product controller

--- server/src/routes/products/productController.ts
+++ server/src/routes/products/productController.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from 'express';
+import { db } from '../../db/index';
+import { productsTable } from '../../db/schema/productsSchema';
 
 export function listProducts(req: Request, res: Response) {
 	res.send('All products');
@@ -6,8 +8,16 @@ export function listProducts(req: Request, res: Response) {
 export function getProductById(req: Request, res: Response) {
 	res.send('getProductById');
 }
-export function createProduct(req: Request, res: Response) {
-	res.send('createProduct');
+export async function createProduct(req: Request, res: Response) {
+	try {
+		const insertedProduct = await db
+			.insert(productsTable)
+			.values(req.body)
+			.returning({ id: productsTable.id, name: productsTable.name });
+		res.status(201).json({ message: 'Product added successfully', product: insertedProduct });
+	} catch (error) {
+		res.status(500).send(error);
+	}
 }
 export function updateProduct(req: Request, res: Response) {
 	res.send('updateProduct');
