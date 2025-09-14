3b85d70525edbe4087547d7cc47c7294940128e7
Sadik-Sami <miasimanto@gmail.com>
2025-09-14 22:25:26 +0600

    Moved to uuid from integers as product ID

--- server/src/db/schema/product.schema.ts
+++ server/src/db/schema/product.schema.ts
@@ -1,12 +1,17 @@
-import { InferModel } from 'drizzle-orm';
-import { doublePrecision, integer, pgTable, text, varchar } from 'drizzle-orm/pg-core';
+import {
+  doublePrecision,
+  pgTable,
+  text,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
 
-export const productsTable = pgTable('products', {
-	id: integer().primaryKey().generatedAlwaysAsIdentity(),
-	name: varchar({ length: 255 }).notNull(),
-	description: text(),
-	image: varchar({ length: 255 }),
-	price: doublePrecision().notNull(),
+export const productsTable = pgTable("products", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: varchar({ length: 255 }).notNull(),
+  description: text("description"),
+  image: varchar("image", { length: 255 }),
+  price: doublePrecision("price").notNull(),
 });
 
 export type Product = typeof productsTable.$inferSelect;
